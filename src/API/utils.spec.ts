11c0906ec8eef2dafa7f44b85959fd0217067796
Jordan Fox <jordan.fox@canstar.com.au>
2023-07-31 22:22:26 +1000

    implement api calls

---
+++ src/API/utils.spec.ts
@@ -0,0 +1,78 @@
+import { describe, it, expect, vi } from 'vitest';
+import { processWeatherDataForWeatherWidgetComponent, getWeatherWidgetDataBasedOnLatLong, getLatLonValuesForInputCity } from './utils';
+import axios from 'axios';
+
+describe('Weather API Utils', () => {
+    describe('getWeatherWidgetDataBasedOnLatLong', () => {
+        it('should call axios get with correct params', async () => {
+            const spy = vi.spyOn(axios, 'get');
+    
+            await getWeatherWidgetDataBasedOnLatLong('New York');
+            // TODO: remove the hardcoded params from this and mock an ENV file
+            expect(spy).toHaveBeenCalledWith(`https://api.openweathermap.org/data/2.5/weather?lat=40.7128&lon=-74.006&appid=9b183e6fa9c718578c431f7b1e2121b8`);
+        });
+        it('should return the fallback response when the function throws an error', async () => {
+            const spy = vi.spyOn(axios, 'get').mockRejectedValue(new Error('Network Error'));
+    
+            const res = await getWeatherWidgetDataBasedOnLatLong('New York');
+            expect(res).toEqual({ temperature: '0', condition: 'N/A', summary: 'No description available, please try again later' });
+        });
+    });
+    
+    describe('processWeatherDataForWeatherWidgetComponent', () => {
+        it('should return default values when response is empty', () => {
+            const response = {};
+    
+            const result = processWeatherDataForWeatherWidgetComponent(response);
+    
+            expect(result.temperature).toEqual('0');
+            expect(result.condition).toEqual('Clear');
+            expect(result.summary).toEqual('No description available');
+        });
+    
+        it('should return correct values when response is valid', () => {
+            const response = {
+                data: {
+                    main: {
+                        temp: 10
+                    },
+                    weather: [
+                        {
+                            main: 'Rain',
+                            description: 'Light rain'
+                        }
+                    ]
+                }
+            };
+    
+            const result = processWeatherDataForWeatherWidgetComponent(response);
+    
+            expect(result.temperature).toEqual('10');
+            expect(result.condition).toEqual('Rain');
+            expect(result.summary).toEqual('Light rain');
+        });
+    });
+    describe('Mapping the city name to the correct lat/lon values', () => {
+        it('should return default values when response is empty', () => {
+            const city = '';
+            const result = getLatLonValuesForInputCity(city);
+            expect(result).toEqual({ lat: 25.761681, lon: -80.191788});
+        });
+        it('should return correct values based on input city miami', () => {
+            const city = 'miami';
+            const result = getLatLonValuesForInputCity(city);
+            expect(result).toEqual({ lat: 25.761681, lon: -80.191788});
+        });
+    
+        it('should return correct values based on input city LA', () => {
+            const city = 'los angeles';
+            const result = getLatLonValuesForInputCity(city);
+            expect(result).toEqual({ lat: 34.0522, lon: -118.2437 });
+        });
+        it('should return correct values based on input city NY', () => {
+            const city = 'new york';
+            const result = getLatLonValuesForInputCity(city);
+            expect(result).toEqual({ lat: 40.7128, lon: -74.0060 });
+        });
+    });
+});
---
+++ src/API/utils.ts
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import ProcessedWeatherData from '../models/ProcessedWeatherData.interface';
+
+export async function getWeatherWidgetDataBasedOnLatLong(cityName: string): Promise<ProcessedWeatherData> {
+    try {
+        const { lat, lon } = getLatLonValuesForInputCity(cityName);
+
+        // WARNING: This is not what I normally do. I have just chosen to expose my API key to make it easier for me to implement this example and will delete this key later.
+        const rawWeatherApiResponseData = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=9b183e6fa9c718578c431f7b1e2121b8`);
+
+        const processedWeatherData = processWeatherDataForWeatherWidgetComponent(rawWeatherApiResponseData);
+        return processedWeatherData;
+    } catch (error) {
+        console.error(error);
+        return { temperature: '0', condition: 'N/A', summary: 'No description available, please try again later' };
+    }
+};
+
+// I didn't think it was safe to guarantee the type of the response, so I left it as `any` and added fallbacks for the data points.
+export function processWeatherDataForWeatherWidgetComponent(weatherApiResponseData: any): ProcessedWeatherData {
+    // I just convert temperature to a string to make life slightly easier when not doing numeric operations
+    const temperature = weatherApiResponseData?.data?.main?.temp.toString() || '0';
+    const condition = weatherApiResponseData?.data?.weather?.[0]?.main || 'Clear';
+    const summary = weatherApiResponseData?.data?.weather?.[0]?.description || 'No description available';
+
+    return { temperature, condition, summary };
+}
+
+export function getLatLonValuesForInputCity(cityName: string) {
+    const cities: { [key: string]: { lat: number; lon: number } } = {
+        'new york': { lat: 40.7128, lon: -74.0060 },
+        'miami': { lat: 25.761681, lon: -80.191788 },
+        'los angeles': { lat: 34.0522, lon: -118.2437 }
+    };
+
+    const normalizedCityName = cityName.toLowerCase();
+    const defaultCity = 'miami';
+    const cityData = cities[normalizedCityName] || cities[defaultCity];
+    return cityData;
+}
