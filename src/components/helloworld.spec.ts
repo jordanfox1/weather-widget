934bbd4212da8b7a2b959b42dbf260c42776688f
Jordan Fox <jordan.fox@canstar.com.au>
2023-07-31 19:23:03 +1000

    configured vue testing lib

--- src/components/helloworld.spec.ts
+++ src/components/helloworld.spec.ts
@@ -1,7 +1,13 @@
 import { describe, it, expect } from 'vitest'
+import { render } from '@testing-library/vue'
+import HelloWorld from './HelloWorld.vue';
 
 describe('code completion', () => {
     it('completes test code', () => {
         expect(true).toBe(true)
+    })
+    it('renders something', async () => {
+        const component = await render(HelloWorld);
+        expect(component).toBeTruthy();
     })
 });
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  /** @ts-expect-error forcing compiler to behave with vitest */
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
   plugins: [vue()],
 })
